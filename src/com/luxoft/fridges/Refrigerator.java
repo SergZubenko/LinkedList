5f08748640f4f275008e52dc432be9ebb3791af1
Serg <sergzubenko@gmail.com>
2017-06-22 02:13:31 +0300

    Some commit

--- src/com/luxoft/fridges/Refrigerator.java
+++ src/com/luxoft/fridges/Refrigerator.java
@@ -12,78 +12,122 @@ public class Refrigerator {
     public static void main(String[] args) {
         Matrix matrix = new Matrix();
         System.out.println(matrix);
-        System.out.println(matrix.switchValue(1,1));
+        System.out.println(matrix.switchValue(1, 1));
         System.out.println(matrix);
     }
 
 
-    public static void resolveMatrix(){
+}
 
 
+class Matrix {
+    private int matrix[][];
 
+    private int size = 4;
+
+
+    int level = 0;
+
+
+    public boolean checkLevel() {
+        if (checkMartix()) {
+            return true;
+        }
+        ;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (switchValue(i, j)) {
+                    System.out.println("x=" + i + "  y=" + j);
+                    return true;
+                } else {
+                    switchValue(i, j);
+                }
+            }
+        }
+        return false;
     }
 
 
-    static class Matrix {
-        private int matrix[][];
+    public boolean resolveMatrix(int upperLevel, int allowedLevel) {
+        int currentLevel = upperLevel + 1;
 
-        private int size = 4;
-
-
-        public Matrix() {
-            matrix = new int[size][size];
-            Random random = new Random();
-
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    matrix[i][j] = random.nextInt(2);
-                }
-            }
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder stringBuilder = new StringBuilder(size * size);
-            stringBuilder.append("[");
-
-            for (int i = 0; i < size; i++) {
-                stringBuilder.append('\n');
-                stringBuilder.append("[");
-                for (int j = 0; j < size; j++) {
-                    stringBuilder.append(matrix[i][j]);
-                    stringBuilder.append('\t');
-                }
-                stringBuilder.append("]");
-            }
-            stringBuilder.append('\n');
-            stringBuilder.append("]");
-            return stringBuilder.toString();
-        }
-
-        public Matrix(int[][] inputArray) {
-            matrix = inputArray;
-        }
-
-        public boolean checkMartix() {
-
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    if (matrix[i][j] == 1) {
-                        return false;
-                    }
-                }
-            }
-
+        if (checkLevel()){
             return true;
         }
 
-        public boolean switchValue(int x, int y){
+
+        for (int l = 0; l < 2; l++) {
             for (int i = 0; i < size; i++) {
-                matrix[x][i] = ~matrix[x][i]+2;
+                for (int j = 0; j < size; j++) {
+                    switchValue(i, j);
+                    if (resolveMatrix(l)) {
+                        return true;
+                    }
+                    switchValue(i, j);
+                }
             }
 
-            return checkMartix();
         }
 
+
+        return false;
     }
+
+
+    public Matrix() {
+        matrix = new int[size][size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = random.nextInt(2);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder(size * size);
+        stringBuilder.append("[");
+
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append('\n');
+            stringBuilder.append("[");
+            for (int j = 0; j < size; j++) {
+                stringBuilder.append(matrix[i][j]);
+                stringBuilder.append('\t');
+            }
+            stringBuilder.append("]");
+        }
+        stringBuilder.append('\n');
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    public Matrix(int[][] inputArray) {
+        matrix = inputArray;
+    }
+
+    public boolean checkMartix() {
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (matrix[i][j] == 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean switchValue(int x, int y) {
+        for (int i = 0; i < size; i++) {
+            matrix[x][i] = ~matrix[x][i] + 2;
+        }
+
+        return checkMartix();
+    }
+
+}
 }
